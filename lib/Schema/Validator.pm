bf8c50d7ead9078c054ee93135e8bc00dfd2bc0d
Nigel Horne <njh@bandsman.co.uk>
2026-06-02 11:06:13 -0400

    pod2markdown

--- lib/Schema/Validator.pm
+++ lib/Schema/Validator.pm
@@ -84,6 +84,8 @@ my %config = (
 # PUBLIC INTERFACE (POD + code)
 # ===========================================================================
 
+=encoding utf-8
+
 =head1 NAME
 
 Schema::Validator - Tools for validating and loading Schema.org vocabulary definitions
@@ -783,19 +785,9 @@ Nigel Horne, C<< <njh at nigelhorne.com> >>
 
 Copyright 2025 Nigel Horne.
 
-Usage is subject to licence terms.
-
-The licence terms of this software are as follows:
-
-=over 4
-
-=item * Personal single user, single computer use: GPL2
-
-=item * All other users (including Commercial, Charity, Educational, Government)
-must apply in writing for a licence for use from Nigel Horne at the
-above e-mail.
-
-=back
+Usage is subject to the GPL2 licence terms.
+If you use it,
+please let me know.
 
 =cut
 
